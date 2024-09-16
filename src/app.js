60735dad9257fb53d0662d4b577308b26ca37538
AlisonCastroM <alisoncastro7616@gmail.com>
2024-09-16 22:03:44 +0000

    primer commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,29 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//import "./assets/img/rigo-baby.jpg";
+//import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const quien = ["Mi perra", "Mi abuela", "Mi novio", "Mi pájaro", "Mi vecino"];
+  const accion = ["se comió", "orinó en", "aplastó", "rompió", "robó"];
+  const que = ["mi tarea", "mi teléfono", "el auto", "mi almuerzo", "mi cama"];
+  const cuando = [
+    "antes de la clase",
+    "cuando estaba durmiendo",
+    "mientras hacía ejercicio",
+    "durante mi almuerzo",
+    "mientras arreglaba mi dormitorio"
+  ];
+
+  function generateExcuse() {
+    const indexQuien = Math.floor(Math.random() * quien.length);
+    const indexAccion = Math.floor(Math.random() * accion.length);
+    const indexQue = Math.floor(Math.random() * que.length);
+    const indexCuando = Math.floor(Math.random() * cuando.length);
+    return `${quien[indexQuien]} ${accion[indexAccion]} ${que[indexQue]} ${cuando[indexCuando]}.`;
+  }
+
+  const excuseElement = document.getElementById("excuse");
+  excuseElement.textContent = generateExcuse();
 };
